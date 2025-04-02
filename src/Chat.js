ab09717d037de69be12cd2bf7549f79f60a96588
Debabrata Halder <108052492+DebabrataHalder@users.noreply.github.com>
2025-04-02 14:03:15 +0530

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -169,11 +169,11 @@ export default function Chat() {
   const [isOpen, setIsOpen] = useState(false);
   const [isConnected, setIsConnected] = useState(false);
   const [credentials, setCredentials] = useState({
-    host: 'mysql-1032ed4a-devhalder321-821fresumemysql.c.aivencloud.com',
-    port: '24736',
-    user: 'avnadmin',
-    password: 'AVNS_Kj2t4KfURIQWNcExquL',
-    database: 'defaultdb',
+    host: 'sql12.freesqldatabase.com',
+    port: '3306',
+    user: 'sql12770829',
+    password: '2ymFE3pAMm',
+    database: 'sql12770829',
   });
   const [connectionStatus, setConnectionStatus] = useState('');
   const [isTesting, setIsTesting] = useState(false);
